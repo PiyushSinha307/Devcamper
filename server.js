187f12f6df0c01da901fbbe11f2250682d3dcbaf
piyushsinha307 <piyushsinha307@gmail.com>
2023-09-16 17:26:58 +0530

    Connected MongoDB database

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
-//Route files
-const bootcamps = require('./routes/bootcamps');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 //Load env vars
 dotenv.config({ path: './config/config.env' });
 
+//Connect to database
+connectDB();
+
+//Route files
+const bootcamps = require('./routes/bootcamps');
+
 const app = express();
 
 //Dev logging Middleware
-if(process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
@@ -20,4 +25,13 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}...`))
+const server = app.listen(PORT,
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}...`.yellow.bold)
+);
+
+//Handle unadleld promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+    //close server & exit process
+    server.close(() => process.exit(1));
+});
